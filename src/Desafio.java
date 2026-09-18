30079b9f7ee0665d6611b50f0cc5061cbd63bcfc
alencarclementino <alencar.clementino@gmail.com>
2026-09-18 13:41:50 -0300

    Initial commit

---
+++ src/Desafio.java
@@ -0,0 +1,9 @@
+public class Desafio {
+    public static void main(String[] args) {
+        double nota1 = 4.5;
+        double nota2 = 5.5;
+        double media = (nota1 + nota2) / 2;
+        System.out.println("total: " + media);
+
+    }
+}
